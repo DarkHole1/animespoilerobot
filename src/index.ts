071cb1ba179397ae4192008f1c31cb793e51e13a
Dark Hole <dark.hole1@yandex.ru>
2023-07-22 16:04:24 +0300

    Testing copying media groups

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,17 @@ import { Config } from "./config"
 const config = Config.loadSync('./config.json')
 const bot = new Bot(config.token)
 
-bot.on('msg', ctx => {
+
+
+bot.on('msg', async ctx => {
     console.log(ctx.msg)
-    return ctx.reply('Gotcha', { reply_to_message_id: ctx.msg.message_id })
+    await ctx.reply('Gotcha', { reply_to_message_id: ctx.msg.message_id })
+    await sleep(1000)
+    await bot.api.copyMessage(ctx.chat.id, ctx.chat.id, ctx.msg.message_id)
 })
 
 bot.start()
+
+function sleep(t: number): Promise<void> {
+    return new Promise(res => setTimeout(res, t))
+}
